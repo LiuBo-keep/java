b297db13ca7c6c2e59e5c4a494afadd5557adbd1
liu <1712624418@qq.com>
2020-04-03 18:08:23 +0800

    springboot_logger

---
+++ springboot_loggin/src/main/java/com/hp/springboot_logg/SpringbootLoggApplication.java
@@ -0,0 +1,13 @@
+package com.hp.springboot_logg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLoggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootLoggApplication.class, args);
+    }
+
+}
