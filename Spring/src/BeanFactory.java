bfca4eaafb91b04e887d56fb74d3b66814a4ec6d
LSJ <1712624418@qq.com>
2021-06-22 07:24:26 +0800

    spring

--- Spring/src/BeanFactory.java
+++ Spring/src/BeanFactory.java
@@ -7,24 +7,50 @@ import exception.NoSuchBeanDefinitionException;
  */
 public interface BeanFactory {
 
-    /***/
+    /**
+     * 获取FactoryBean本身
+     */
     String FACTORY_BEAN_PREFIX = "&";
 
+    /**
+     * 通过指定名称来获取对应的bean
+     */
     Object getBean(String name) throws BeanEception;
 
+    /**
+     * 通过指定名称(name)同时指定所需类型(requiredType)获取对应的bean
+     */
     <T> T getBean(String name, Class<T> requiredType) throws BeanEception;
 
     Object getBean(String name, Object... args) throws BeanEception;
 
+    /**
+     * 判断容器中是否含有指定名字的bean
+     */
     boolean containsBean(String name);
 
+    /**
+     * 查询指定名字的bean是否是单例类型的bean，对于Singleton属性，可以在BeanDefinition中指定
+     */
     boolean isSingLeton(String name) throws NoSuchBeanDefinitionException;
 
+    /**
+     * 查询指定名字的Bean是否是prototype类型的
+     */
     boolean isPrototype(String name) throws NoSuchBeanDefinitionException;
 
+    /**
+     * 查询指定名字的bean的Class类型是否是特定的Class类型，这个类型由用户指定
+     */
     boolean isTypeMatch(String name, Class targetType) throws NoSuchBeanDefinitionException;
 
+    /**
+     * 查询指定名字的bean的Class类型
+     */
     Class getType(String name) throws NoSuchBeanDefinitionException;
 
+    /**
+     * 查询指定名字的bean的所有别名，这些别名都是用户在BeanDefinition中定义的
+     */
     String[] getAliases(String name);
 }
